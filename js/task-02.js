80ef5e0fa4383ff9fcd4e09ac4cc1eb331692e4c
Iryna_Kobyzhcha <irinakobyzhcha16@ukr.net>
2023-03-11 22:40:16 +0200

    add task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newListIngredients = ingredients.map(elem => {
+	const item = document.createElement("li");
+	item.textContent = elem;
+	item.classList.add('item');
+	return item;
+	}
+);
+
+console.log(newListIngredients);
+document.querySelector('#ingredients').append(...newListIngredients);
