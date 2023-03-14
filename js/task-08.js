347fb072d17fbb8a077c45ccf28e6518894df707
Iryna_Kobyzhcha <irinakobyzhcha16@ukr.net>
2023-03-14 13:08:47 +0200

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+  event.preventDefault(); 
+
+  const formElements = event.currentTarget.elements;   
+  const email = formElements.email.value;
+  const password= formElements.password.value;
+
+const formData = {
+  email,
+  password,
+}
+
+if (email === "") {
+  alert("Введіть, будь ласка, електронну адресу");
+} else if (password === "") {
+  alert("Введіть, будь ласка, пароль.");
+} else {
+  console.log(formData);
+  formEl.reset();
+  console.log(formEl);
+}  
+}
+
+//   const formData = new FormData(event.currentTarget);
+// formData.forEach((value, name) => {
+//   console.log(value);
+//   console.log(name);
+// });
+// console.log(formData);
+  // console.dir(formElements);
