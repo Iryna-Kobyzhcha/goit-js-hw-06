71185e4e94db2d8394109a9aea3878e1413e713d
Iryna_Kobyzhcha <irinakobyzhcha16@ukr.net>
2023-03-14 15:55:09 +0200

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const elBody = document.querySelector("body");
+const elSpanCurrentColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", () => {
+	const backgroundColor = getRandomHexColor();
+	elBody.style.backgroundColor = `${backgroundColor}`;
+	elSpanCurrentColor.textContent = backgroundColor;
+})
