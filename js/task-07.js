9c3e7fd5841a1d0a0390b9fa4ff29511307ea5ab
Iryna_Kobyzhcha <irinakobyzhcha16@ukr.net>
2023-03-16 17:12:07 +0200

    Fixed mistakes

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const styleInput = document.querySelector('#font-size-control');
 const styleText = document.querySelector('#text');
 
+styleText.style.fontSize = `${styleInput.value}px`;
+
 styleInput.addEventListener("input", (event) => {
 	styleText.style.fontSize = `${styleInput.value}px`;
 })
